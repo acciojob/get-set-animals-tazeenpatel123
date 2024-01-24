790f02f8a8635578f49ab0b057a23dd100fd7545
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2024-01-24 16:34:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //complete this code
-class Animal {
+class Animal 
 	constructor(species){
 		this._species=species;
 	}
@@ -7,14 +7,12 @@ class Animal {
 		  return this._species;
 	}
 	 makeSound(){
-		console.log(`The ${species} makes a sound`);
+		console.log(`The ${this._species} makes a sound`);
 	}
 }
 
 class Dog extends Animal {
-	constructor(species){
-		super(species);
-	}
+	
 	bark() {
     console.log("woof");
   }
@@ -22,9 +20,7 @@ class Dog extends Animal {
 }
 
 class Cat extends Animal {
-	constructor(species){
-		super(species);
-	}
+	
 	purr() {
     console.log("purr");
   }
